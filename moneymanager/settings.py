f10ddf4ea9b802a1e5ba881e539a6d942a62cd00
Shivtej Deshmukh <shivtejhd@gmail.com>
2025-04-03 14:57:34 +0530

    Fixed Issues

--- moneymanager/settings.py
+++ moneymanager/settings.py
@@ -126,6 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
+LOGIN_URL = "/login/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
